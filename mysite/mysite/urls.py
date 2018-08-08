e5789e11ee4a59b79d59b227fbcb5e2cbf065c20
Francisco <fvcneto.master98@gmail.com>
2018-08-08 11:42:53 -0300

    change main path

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('twpy.urls')),
+    path('/', include('twpy.urls')),
     path('admin/', admin.site.urls),
 ]
